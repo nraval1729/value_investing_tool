50a20e89ec1fabaada4ff4062e3cf35d9b631eec
Nisarg Rawal <nisarg_rawal@brown.edu>
2017-03-20 16:31:48 -0400

    Added functionality to update current.json every 5 minutes upon server launch

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Exress shit
+// Express shit
 var express = require('express');
 var app = express();
 
@@ -22,33 +22,45 @@ var util = require('util');
 // Python bridge to run python from node
 var spawn = require("child_process").spawn;
 
-// Home page handler
-app.get("/", function(req, res) {
-	filePath = __dirname + "/public/scripts/python/biographical_historical_scraper.py";
-	console.log("FIlepath: " +filePath);
-	var process = spawn('python',[filePath]);
+
+// This chunk of code spawns a new python child process that launches the current_scraper.py to update the current.json every 5 mins
+filePath = __dirname + "/public/scripts/python/current_scraper.py"
+console.log("File path: " +filePath);
+var process = spawn('python',[filePath]);
 	process.stdout.on('data', function(data) {
 		console.log("Received: " +data);
 	});
+
+// Home page handler
+app.get("/", function(req, res) {
+
+	// Spawns a new python child process that launches the biographical_historical_scraper.py to update biographical.json and historical.json
+	var scriptPath = __dirname + "/public/scripts/python/biographical_historical_scraper.py";
+	var process = spawn('python',[scriptPath]);
+	process.stdout.on('data', function(data) {
+		console.log("Received: " +data);
+	});
+
+	// Renders the main page
 	res.render('index.html');
 });
 
 app.get("/current", function(req, res) {
-	filePath = __dirname + '/public/json_files/current.json';
-	console.log("Inside /current, sending current json at path: " +filePath);
-	res.sendFile(filePath);
+	currentFilePath = __dirname + '/public/json_files/current.json';
+	console.log("Inside /current, sending current json at path: " +currentFilePath);
+	res.sendFile(currentFilePath);
 });
 
 app.get("/historical", function(req, res) {
-	filePath = __dirname + '/public/json_files/historical.json';
-	console.log("Inside /current, sending historical json at path: " +filePath);
-	res.sendFile(filePath);
+	historicalFilePath = __dirname + '/public/json_files/historical.json';
+	console.log("Inside /historical, sending historical json at path: " +historicalFilePath);
+	res.sendFile(historicalFilePath);
 });
 
 app.get("/biographical", function(req, res) {
-	filePath = __dirname + '/public/json_files/biographical.json';
-	console.log("Inside /current, sending biographical json at path: " +filePath);
-	res.sendFile(filePath);
+	biographicalFilePath = __dirname + '/public/json_files/biographical.json';
+	console.log("Inside /biographical, sending biographical json at path: " +biographicalFilePath);
+	res.sendFile(biographicalFilePath);
 });
 
 app.listen(8080)
