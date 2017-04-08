645b057a94b2efe76a3bd08148325b03a4a82b06
Linda Park <linda_park@brown.edu>
2017-04-08 14:43:06 -0400

    Front-end update #3

--- public/scripts/javascript/index-ver2.js
+++ public/scripts/javascript/index-ver2.js
@@ -1,32 +1,7 @@
-// // SEARCH
-// $('#search').keyup(function() {
-//   var searchField = $('#search').val();
-//   var userInput = new RegExp(searchField, "i");
-//     $.getJSON('http://valueinvestingtool.com/practice/historical.json', function(data) {
-//       var searchOutput = '<ul class="searchResults">';
-//       $.each(data, function(key, val) {
-//         if ((val.ticker.search(userInput) != -1)) {
-//           searchOutput += '<li>';
-//           searchOutput += '<p>' + val.ticker + '</p>';
-//           searchOutput += '<p>' + val.pe_avg + '</p>';
-//           searchOutput += '<p>' + val.ps_avg + '</p>';
-//           searchOutput += '<p>' + val.pb_avg + '</p>';
-//           searchOutput += '<p>' + val.div_avg + '</p>';
-//           searchOutput += '</li>';
-//         }
+$(function() {
+  sector(biographical);
+});
 
-//       });
-//       searchOutput += '</ul>';
-//       if(searchField!="") $('#update').html(searchOutput); //output result to the update div
-//       else $('#update').html('');
-//     });
-// });
-
-  $(function() {
-    var sectorName = sector(biographical);
-    var industryName = industry(biographical, sectorName);
-    company(biographical, technical, industryName);
-  });
 /*
 *
 *
@@ -34,11 +9,7 @@
 *
 *
 */
-//$.get('http://valueinvestingtool.com/practice-linda/biographical.json', function(data) {
-//$.get('/Users/lindapark/Desktop/value_investing_tool/public/json_files/biographical.json', function(data) {
-
-  //console.log("data" + data);
-  function sector(data) {
+function sector(data) {
   var data = biographical;
   var listOfSectors = [];
   var uniqueSectors = [];
@@ -95,242 +66,226 @@
 
     // ...update hash in URL (for SPA - Single Page App - purposes)
     window.location.hash = $(this).attr('href');
-    return nameOfSector;
+    industry(biographical, nameOfSector);
   });
 
 }
 
-  /*
-  *
-  *
-  *** INDUSTRIES
-  *
-  *
-  */
-  //$.getJSON('http://valueinvestingtool.com/practice-linda/biographical.json', function(data) {
-    function industry(data, sector) {
-    var sectorName = sector;
-    var data = biographical;
-    var matchingSectors = [];
-    var uniqueIndustries = [];
-    var listOfUniqueIndustries = [];
+/*
+*
+*
+*** INDUSTRIES
+*
+*
+*/
+function industry(data, sector) {
+  var sectorName = sector;
+  var data = biographical;
+  var matchingSectors = [];
+  var uniqueIndustries = [];
+  var listOfUniqueIndustries = [];
 
-    // Step 1: find matching sectors from biographical.json
-    // For example:
-    // If user selects "Health Care" from the list of sectors, any biographical.json object that has "Health Care" as sector will be pushed into matchingSectors array.
-    //[
-    // {
-    //     "ticker": "A", 
-    //     "security": "Agilent Technologies Inc", 
-    //     "sector": "Health Care", 
-    //     "industry": "Health Care Equipment"
-    // },  ---> this whole object in {} will be pushed into matchingSectors array.
-    // ...
-    //]
-    for (var i = 0; i < data.length; i++) {
-      if (sectorName == data[i]['sector'] && !(sectorName in matchingSectors)) { // if sector names match and not already in a list of cells
-        matchingSectors.push(data[i]);
-      }
+  // Step 1: find matching sectors from biographical.json
+  // For example:
+  // If user selects "Health Care" from the list of sectors, any biographical.json object that has "Health Care" as sector will be pushed into matchingSectors array.
+  //[
+  // {
+  //     "ticker": "A", 
+  //     "security": "Agilent Technologies Inc", 
+  //     "sector": "Health Care", 
+  //     "industry": "Health Care Equipment"
+  // },  ---> this whole object in {} will be pushed into matchingSectors array.
+  // ...
+  //]
+  for (var i = 0; i < data.length; i++) {
+    if (sectorName == data[i]['sector'] && !(sectorName in matchingSectors)) { // if sector names match and not already in a list of cells
+      matchingSectors.push(data[i]);
     }
-    
-    // Step 2: there can be repeating industries from list of matching sectors, so consider duplicate industries only once
-    // For example:
-    //[
-    // {
-    //     "ticker": "A", 
-    //     "security": "Agilent Technologies Inc", 
-    //     "sector": "Health Care", 
-    //     "industry": "Health Care Equipment"
-    // }, 
-    // {
-    //     "ticker": "ABT", 
-    //     "security": "Abbott Laboratories", 
-    //     "sector": "Health Care", 
-    //     "industry": "Health Care Equipment"
-    // }, 
-    // ...
-    //] --> both objects identify their industries as "Health Care Equipment", so count that only once and put it in uniqueIndustries array
-    uniqueIndustries = matchingSectors.filter((set => f => !set.has(f.industry) && set.add(f.industry))(new Set));
-
-    // Step 3: extract only industries from each
-    //[
-    // {
-    //     "ticker": "A", 
-    //     "security": "Agilent Technologies Inc", 
-    //     "sector": "Health Care", 
-    //     "industry": "Health Care Equipment"
-    // }, 
-    // {
-    //     "ticker": "ABBV", 
-    //     "security": "AbbVie", 
-    //     "sector": "Health Care", 
-    //     "industry": "Pharmaceuticals"
-    // }, 
-    // ...
-    //] --> we don't need each object in their entirety; rather, we only need their industry extracted so that it results in sth like...
-    // ["Health Care Equipment", "Pharmaceuticals", ...] --> this is listOfUniqueIndustries.
-    for (var i = 0; i < uniqueIndustries.length; i++) {
-      listOfUniqueIndustries.push(uniqueIndustries[i]['industry']);
-    }
-
-    // Step 4: create array from stringified JSON object
-    // Before splitting string, listOfUniqueIndustries[0] returns [, [1] returns ", and [2] returns H
-    JSON.stringify(listOfUniqueIndustries).split(',');
-    // After splitting, listOfUniqueIndustries[0] returns "Health Care Equipment" and [1] returns "Pharmaceuticals" ... which is what we want!
-
-    // Step 5: construct industry table
-    var industryTable = $("#industry");
-    var numCol = 4; // limiting to 4 cols for visual aesthetics
-    var numRow = Math.ceil(listOfUniqueIndustries.length / 4);
-    var urlIndustries = generateHashURL(listOfUniqueIndustries); 
-    var index = 0;
-
-    for (var i = 0; i < numRow; i++) {
-      industryTable.append('<tr>');
-      for (var j = 0; j < numCol; j++) {
-        industryTable.append('<td>');
-        industryTable.append("<a href='" + urlIndustries[index] + "' class = 'aIndustry'>" + listOfUniqueIndustries[index] + '</a>');
-        $('a:contains("undefined")').remove(); // delete any excess empty links that come out as "undefined"
-        index++;
-        industryTable.append('</td>');
-      }
-      industryTable.append('</tr>');
-    }
-    
-    // When user clicks on one of industries on table...
-    $('a.aIndustry').click( function(e) {
-      e.preventDefault(); 
-      // ...replace heading with user-clicked sector
-      var nameOfIndustry = $(this).text();
-      $('h2').text($(this).text());
-
-      // ...hide the sector table so that industry table can be placed where it was
-      industryTable.hide();
-
-      // ...update hash in URL (for SPA - Single Page App - purposes)
-      window.location.hash = $(this).attr('href');
-      //var listOfTickers = [];
-      return nameOfIndustry;
-      });
   }
-      /*
-      *
-      *
-      *** COMPANIES
-      *
-      *
-      */
-      //$.getJSON('http://valueinvestingtool.com/practice-linda/biographical.json', function(data) {
-        function company(biographicaljson, technicaljson, industry) {
-        var biographical = biographicaljson;
-        var matchingIndustries = [];
-        var uniqueCompanies = [];
-        var listOfUniqueCompanies = [];
-        var listOfTickers = [];
+  
+  // Step 2: there can be repeating industries from list of matching sectors, so consider duplicate industries only once
+  // For example:
+  //[
+  // {
+  //     "ticker": "A", 
+  //     "security": "Agilent Technologies Inc", 
+  //     "sector": "Health Care", 
+  //     "industry": "Health Care Equipment"
+  // }, 
+  // {
+  //     "ticker": "ABT", 
+  //     "security": "Abbott Laboratories", 
+  //     "sector": "Health Care", 
+  //     "industry": "Health Care Equipment"
+  // }, 
+  // ...
+  //] --> both objects identify their industries as "Health Care Equipment", so count that only once and put it in uniqueIndustries array
+  uniqueIndustries = matchingSectors.filter((set => f => !set.has(f.industry) && set.add(f.industry))(new Set));
 
-        // Step 1: find all companies that match to their respective industry
-        //
-        for (var i = 0; i < biographical.length; i++) {
-          if (industry == biographical[i]['industry'] && !(nameOfIndustry in matchingIndustries)) { // if industry names match and not already in a list of cells
-            matchingIndustries.push(biographical[i]);
-          }
-        }
+  // Step 3: extract only industries from each
+  //[
+  // {
+  //     "ticker": "A", 
+  //     "security": "Agilent Technologies Inc", 
+  //     "sector": "Health Care", 
+  //     "industry": "Health Care Equipment"
+  // }, 
+  // {
+  //     "ticker": "ABBV", 
+  //     "security": "AbbVie", 
+  //     "sector": "Health Care", 
+  //     "industry": "Pharmaceuticals"
+  // }, 
+  // ...
+  //] --> we don't need each object in their entirety; rather, we only need their industry extracted so that it results in sth like...
+  // ["Health Care Equipment", "Pharmaceuticals", ...] --> this is listOfUniqueIndustries.
+  for (var i = 0; i < uniqueIndustries.length; i++) {
+    listOfUniqueIndustries.push(uniqueIndustries[i]['industry']);
+  }
 
-        // Step 2: remove duplicate companies
-        //
-        uniqueCompanies = matchingIndustries.filter((set => f => !set.has(f.security) && set.add(f.security))(new Set));
+  // Step 4: create array from stringified JSON object
+  // Before splitting string, listOfUniqueIndustries[0] returns [, [1] returns ", and [2] returns H
+  JSON.stringify(listOfUniqueIndustries).split(',');
+  // After splitting, listOfUniqueIndustries[0] returns "Health Care Equipment" and [1] returns "Pharmaceuticals" ... which is what we want!
 
-        // Step 3: extract only company name from the list of json objects
-        //
-        for (var i = 0; i < uniqueCompanies.length; i++) {
-          listOfUniqueCompanies.push(uniqueCompanies[i]['security']);
-        }
+  // Step 5: construct industry table
+  var industryTable = $("#industry");
+  var numCol = 4; // limiting to 4 cols for visual aesthetics
+  var numRow = Math.ceil(listOfUniqueIndustries.length / 4);
+  var urlIndustries = generateHashURL(listOfUniqueIndustries); 
+  var index = 0;
 
-        // Step 4: format each JSON string to array separated by commas
-        JSON.stringify(listOfUniqueCompanies).split(',');
+  for (var i = 0; i < numRow; i++) {
+    industryTable.append('<tr>');
+    for (var j = 0; j < numCol; j++) {
+      industryTable.append('<td>');
+      industryTable.append("<a href='" + urlIndustries[index] + "' class = 'aIndustry'>" + listOfUniqueIndustries[index] + '</a>');
+      $('a:contains("undefined")').remove(); // delete any excess empty links that come out as "undefined"
+      index++;
+      industryTable.append('</td>');
+    }
+    industryTable.append('</tr>');
+  }
+  
+  // When user clicks on one of industries on table...
+  $('a.aIndustry').click( function(e) {
+    e.preventDefault(); 
+    // ...replace heading with user-clicked sector
+    var nameOfIndustry = $(this).text();
+    $('h2').text($(this).text());
 
-        // Step 5: Construct a list of corresponding tickers for each company name in array
-        for (var i = 0; i < listOfUniqueCompanies.length; i++) {
-          if (listOfUniqueCompanies[i] == uniqueCompanies[i]['security']) {
-            listOfTickers.push(uniqueCompanies[i]['ticker']);
-          }
-        }
+    // ...hide the sector table so that industry table can be placed where it was
+    industryTable.hide();
 
-        // Step 6: format each JSON string to array separated by commas
-        JSON.stringify(listOfTickers).split(','); // This is splitted
+    // ...update hash in URL (for SPA - Single Page App - purposes)
+    window.location.hash = $(this).attr('href');
+    company(biographical, technical, nameOfIndustry);
+    });
+  }
+/*
+*
+*
+*** COMPANIES
+*
+*
+*/
+function company(biographicaljson, technicaljson, industryName) {
+  var industry = industryName;
+  var biographical = biographicaljson;
+  var matchingIndustries = [];
+  var uniqueCompanies = [];
+  var listOfUniqueCompanies = [];
+  var listOfTickers = [];
 
-        // Step 7: store length of listOfTickers for reference later
-        var numListOfTickers = listOfTickers.length;
+  // Step 1: find all companies that match to their respective industry
+  //
+  for (var i = 0; i < biographical.length; i++) {
+    if (industry == biographical[i]['industry'] && !(industry in matchingIndustries)) { // if industry names match and not already in a list of cells
+      matchingIndustries.push(biographical[i]);
+    }
+  }
 
-        /*
-        *
-        *
-        *** GET REQUEST ON TECHNICAL.JSON
-        *
-        *
-        */
-        //$.getJSON('http://valueinvestingtool.com/practice-linda/technical.json', function(data) {
-          var technical = technicaljson;
-          var pe_cur = [];
-          var pe_avg = [];
-          var ps_cur = [];
-          var ps_avg = [];
-          var pb_cur = [];
-          var pb_avg = [];
-          var div_cur = [];
-          var div_avg = [];
-          var s_rank = [];
-          var matchingTickers = [];
+  // Step 2: remove duplicate companies
+  //
+  uniqueCompanies = matchingIndustries.filter((set => f => !set.has(f.security) && set.add(f.security))(new Set));
 
-          // Since technical.json does not have company name listed, use list of tickers
-          for (var x = 0; x < numListOfTickers; x++) {
-            for (var y = 0; y < technical.length; y++) {
-              if (listOfTickers[x] == technical[y]['ticker']) {
-                matchingTickers.push(technical[y]);
-              }
-            }
-          }
-          JSON.stringify(matchingTickers).split(',');
+  // Step 3: extract only company name from the list of json objects
+  //
+  for (var i = 0; i < uniqueCompanies.length; i++) {
+    listOfUniqueCompanies.push(uniqueCompanies[i]['security']);
+  }
 
-          // Store values of current and historical.
-          for (var i = 0; i < matchingTickers.length; i++) {
-            console.log("there's a match!");
-            pe_cur.push(matchingTickers[i]['pe_cur']);
-            pe_avg.push(matchingTickers[i]['pe_avg']);
+  // Step 4: format each JSON string to array separated by commas
+  JSON.stringify(listOfUniqueCompanies).split(',');
 
-            ps_cur.push(matchingTickers[i]['ps_cur']);
-            ps_avg.push(matchingTickers[i]['ps_avg']);
+  // Step 5: Construct a list of corresponding tickers for each company name in array
+  for (var i = 0; i < listOfUniqueCompanies.length; i++) {
+    if (listOfUniqueCompanies[i] == uniqueCompanies[i]['security']) {
+      listOfTickers.push(uniqueCompanies[i]['ticker']);
+    }
+  }
 
-            pb_cur.push(matchingTickers[i]['pb_cur']);
-            pb_avg.push(matchingTickers[i]['pb_avg']);
+  // Step 6: format each JSON string to array separated by commas
+  JSON.stringify(listOfTickers).split(','); // This is splitted
 
-            div_cur.push(matchingTickers[i]['div_cur']);
-            div_avg.push(matchingTickers[i]['div_avg']);
+  // Step 7: store length of listOfTickers for reference later
+  var numListOfTickers = listOfTickers.length;
 
-            s_rank.push(matchingTickers[i]['s_rank']);
-          }
+  var technical = technicaljson;
+  var pe_cur = [];
+  var pe_avg = [];
+  var ps_cur = [];
+  var ps_avg = [];
+  var pb_cur = [];
+  var pb_avg = [];
+  var div_cur = [];
+  var div_avg = [];
+  var s_rank = [];
+  var matchingTickers = [];
 
-          // Construct company table
-          var companyTable = $("#company");
+  // Since technical.json does not have company name listed, use list of tickers
+  for (var x = 0; x < numListOfTickers; x++) {
+    for (var y = 0; y < technical.length; y++) {
+      if (listOfTickers[x] == technical[y]['ticker']) {
+        matchingTickers.push(technical[y]);
+      }
+    }
+  }
+  JSON.stringify(matchingTickers).split(',');
 
-          for (var index = 0; index < listOfUniqueCompanies.length; index++) {
+  // Store values of current and historical.
+  for (var i = 0; i < matchingTickers.length; i++) {
+    pe_cur.push(matchingTickers[i]['pe_cur']);
+    pe_avg.push(matchingTickers[i]['pe_avg']);
 
-            companyTable.append(
-              '<tr><td>' + listOfUniqueCompanies[index] + '</td>' + 
-              '<td>' + pe_cur[index] + '</td>' + 
-              '<td>' + ps_cur[index] + '</td>' + 
-              '<td>' + pb_cur[index] + '</td>' + 
-              '<td>' + div_cur[index] + '</td>' + 
-              '<td>' + s_rank[index] + '</td>' +
-              '</tr>');
-          }
-        //});
-      //});
-    //});
-  //});
+    ps_cur.push(matchingTickers[i]['ps_cur']);
+    ps_avg.push(matchingTickers[i]['ps_avg']);
+
+    pb_cur.push(matchingTickers[i]['pb_cur']);
+    pb_avg.push(matchingTickers[i]['pb_avg']);
+
+    div_cur.push(matchingTickers[i]['div_cur']);
+    div_avg.push(matchingTickers[i]['div_avg']);
+
+    s_rank.push(matchingTickers[i]['s_rank']);
+  }
+
+  // Construct company table
+  var companyTable = $("#company");
+
+  for (var index = 0; index < listOfUniqueCompanies.length; index++) {
+
+    companyTable.append(
+      '<tr><td>' + listOfUniqueCompanies[index] + '</td>' + 
+      '<td>' + pe_cur[index] + '</td>' + 
+      '<td>' + ps_cur[index] + '</td>' + 
+      '<td>' + pb_cur[index] + '</td>' + 
+      '<td>' + div_cur[index] + '</td>' + 
+      '<td>' + s_rank[index] + '</td>' +
+      '</tr>');
+  }
   return false; 
 }
-//});
 
 // GENERATING HASH URL
 function generateHashURL(list) {
@@ -345,8 +300,6 @@ function generateHashURL(list) {
 if (window.performance) {
   window.location.hash = "";
 }
-
-//});
 
 // Sector -> industry -> company
 
@@ -6219,4 +6172,3 @@ var technical = [
         "ps_avg": "4.2"
     }
 ];
-//});
