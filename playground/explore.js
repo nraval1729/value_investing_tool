777d5c4604e6a6f6b49769ddff916c96e41521cd
craighawkins7 <craig_hawkins@brown.edu>
2017-04-11 15:07:15 -0400

    css harmonized, minor cleanup...

--- playground/explore.js
+++ playground/explore.js
@@ -129,7 +129,7 @@ function renderIndustries(data, sector) {
     // Step 4: create array from stringified JSON object
     // Before splitting string, listOfUniqueIndustries[0] returns [, [1] returns ", and [2] returns H
     JSON.stringify(listOfUniqueIndustries).split(',');
-    // After splitting, listOfUniqueIndustries[0] returns "Health Care Equipment" and [1] returns "Pharmaceuticals" ... which is what we want!
+    // After splitting, listOfUniqueIndustries[0] returns "Health Care Equipment" and [1] returns "Pharmaceuticals" ... which is what we want.
 
     // Step 5: construct industry table
     var numRows = Math.ceil(listOfUniqueIndustries.length / NUM_COLS);
@@ -165,7 +165,6 @@ function renderIndustries(data, sector) {
         var sectorLink = '<a href=' + window.location.hash + '>' + sectorName + '</a>';
         $('#sectorCrumb').html(sectorLink);
 
-
         // ...hide the sector table so that industry table can be placed where it was
         sectorTable.hide();
         industryTable.hide();
@@ -194,7 +193,7 @@ function renderCompanies(biographicaljson, technicaljson, industryName, sectorNa
     }
     
 
-        // When user clicks on one of industries on table...
+    // When user clicks on one of industries on table...
     $('#sectorCrumb').click( function(e) {
         e.preventDefault(); 
 
@@ -283,13 +282,3 @@ function generateHashURL(list) {
 if (window.performance) {
     window.location.hash = "";
 }
-
-// Sector -> industry -> company
-
-// color = ([pe or ps or div]_avg - [pe or ps or div]_curr) / ([pe or ps or div]_std)
-
-// -2 bright green, -1 green // 0 black // +1 red, +2 bright red
-
-// pe, ps, pb = +1, +2 red; -1, -2 green
-
-// div = +1, +2 green; -1, -2 red
