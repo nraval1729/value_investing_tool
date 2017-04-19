b2d926576666f96cfe6a804e2dadf609edbbac80
craighawkins7 <craig_hawkins@brown.edu>
2017-04-19 15:18:06 -0400

    new file

---
+++ public/scripts/javascript/spa.js
@@ -0,0 +1,27 @@
+
+
+//alert("hello from spa.js");
+
+
+$(document).ready(function() {
+});
+
+function showExplore() {
+    $("#exploreSection").toggle(true);
+    $("#searchSection").toggle(false);
+}
+
+function showSearch() {
+    $("#searchSection").toggle(true);
+    $("#exploreSection").toggle(false);
+}
+
+function doSomething() {
+    alert("I did something");
+}
+
+$(document).ready(function() {
+    $('#searchBarInput').keypress(function (e) {
+        if (e.which == 13) doSomething();
+    });
+});
